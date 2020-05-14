d67845b46346b3bf898bf46b1f3cf673f4ed71c8
Oh-MyBug <justleecong@163.com>
2020-05-14 11:34:54 +0800

    update EXP_3 src

--- EXP_3/src/fork-exec-demo.c
+++ EXP_3/src/fork-exec-demo.c
@@ -7,10 +7,10 @@ int main(int argc, char ** argv ) {
 		printf("error!\n");
 	} else if( pid ==0 ) {
 		printf("This is the child process!\n");
-		char *argv[ ]={"ls", "-al", "/etc/passwd", NULL};
-		char *envp[ ]={"PATH=/usr/bin", NULL};                 
-		execve("/usr/bin/ls", argv, envp);
-		printf("this printf()will not be exec,because it will be replaced by /usr/bin/ls code!\n");
+		char *argv[]={"pstree", "2536", NULL, NULL};
+		char *envp[]={"PATH=/usr/bin", NULL};                 
+		execve("/usr/bin/pstree", argv, envp);
+		printf("this printf()will not be exec,because it will be replaced by /usr/bin/pstree code!\n");
 	} else {
 		printf("This is the parent process! child process id = %d\n", pid);
 		getchar();         
