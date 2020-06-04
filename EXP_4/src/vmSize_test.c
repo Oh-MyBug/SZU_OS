c00b5239bc9609271ee68fc00f63500836491769
Oh-MyBug <justleecong@163.com>
2020-06-04 13:27:16 +0800

    update

--- EXP_4/src/vmSize_test.c
+++ EXP_4/src/vmSize_test.c
@@ -5,12 +5,13 @@
 
 int main(){
 	int MAX_NUM = 100000;
+	int i;
 	char *m[MAX_NUM];
 	printf("Process %d has been generated!\n", getpid());
 	getchar();
-	for(int i = 0; i < MAX_NUM; i ++){
-		p[i] = (char *)malloc(pow(2,30)*sizeof(char)); // 分配1G内存
-		if(p[i] == NULL){
+	for(i = 0; i < MAX_NUM; i ++){
+		m[i] = (char *)malloc(pow(2,30)*sizeof(char)); // 分配1G内存
+		if(m[i] == NULL){
 			printf("Run out of virtual memory!\n");
 			getchar();
 			break;
